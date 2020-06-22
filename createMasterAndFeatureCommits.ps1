48fe228677fc7d245b8b325e56c0c4e1669dd84f
Mark McFadden <m2web@yahoo.com>
2020-06-22 13:35:34 -0400

    C# Class, PowerShell & Bash scripts, Internals Doc

---
+++ createMasterAndFeatureCommits.ps1
@@ -0,0 +1,41 @@
+mkdir test
+cd test
+
+#remove git folder if present
+IF(Test-Path .git){
+	#Write-Output "got git"
+	rm .git -r -fo
+	# -r (recursive). Note that -f in PowerShell is ambiguous for -Filter and -Force and thus -fo needs to be used.
+	rm *
+} 
+
+git init
+
+#number of commits on the master branch
+$numberOfMasterCommitsArray = @("M1", "M2", "M3")
+ 
+for($i = 0; $i -lt $numberOfMasterCommitsArray.length; $i++){ 
+	$numberOfMasterCommitsArray[$i] | Out-File $numberOfMasterCommitsArray[$i]
+	git add .
+	git commit -m $numberOfMasterCommitsArray[$i]
+}
+
+git checkout -b feature
+
+#number of commits on the feature branch
+$numberOfFeaturesCommitsArray = @("F1", "F2", "F3", "F4")
+ 
+for($i = 0; $i -lt $numberOfFeaturesCommitsArray.length; $i++){ 
+	$numberOfFeaturesCommitsArray[$i] | Out-File $numberOfFeaturesCommitsArray[$i]
+	git add .
+	git commit -m $numberOfFeaturesCommitsArray[$i]
+}
+
+#in a separte terminal run this script to view the .git structure updates
+#DO
+#{
+#clear
+#tree .git
+#Start-Sleep -s 2
+#}
+#While (1 -eq 1)
---
+++ mycode.cs
@@ -0,0 +1,45 @@
+public class MyClass {
+
+    public string Name {get; set;}
+    public string Address {get; set;}
+
+    public void MethodZero(){
+        //some code here
+    }
+    
+    public void MethodOne(){
+        //some code here
+    }
+
+    public void MethodOnePointFive(){
+        //some code here
+    }
+
+    public void MethodTwo(){
+        //some code here
+    }
+    
+    public void MethodTwoPointFive(){
+        //some code here
+    }
+
+    public void MethodThree(){
+        //some code here
+    }
+
+    public void MethodThreePointFive(){
+        //some code here
+    }
+
+    public void MethodFour(){
+        //some code here
+    }
+
+    public void MethodFive(){
+        //some code here
+    }
+
+    public void MethodSix(){
+        //some code here
+    }
+}
