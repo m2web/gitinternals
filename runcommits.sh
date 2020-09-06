ae24b30dee4fe1d337f625f4fbde5a790f02a084
Mark McFadden <m2web@yahoo.com>
2020-09-06 14:40:35 -0400

    Made file executable

--- runcommits.sh
+++ runcommits.sh
@@ -19,9 +19,16 @@ done
 
 git checkout -b feature
 
-for i in 1 2 3 4
+for i in 1 2
 do
   echo "F$i" > F$i.txt
   git add F$i.txt
   git commit -m "F$i"
+done
+
+for i in 5 6 7
+do
+  echo "M$i" > M$i.txt
+  git add M$i.txt
+  git commit -m "M$i"
 done
