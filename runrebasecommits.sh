85d420f20b9d0ce6865344e755781cccdba6f937
Mark McFadden <m2web@yahoo.com>
2020-09-07 10:43:30 -0400

    New rebase commit script

---
+++ runrebasecommits.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#remove existing .git data
+#rm -rf .git
+#remove existing txt files
+#rm *.txt
+
+#reinitialize git
+#git init
+
+#the hooks scripts are noise here; remove them
+#rm .git/hooks/*
+
+for i in 1 2 3
+do
+  echo "M$i" > M$i.txt
+  git add M$i.txt
+  git commit -m "M$i"
+done
+
+git checkout -b feature
+
+for i in 1 2
+do
+  echo "F$i" > F$i.txt
+  git add F$i.txt
+  git commit -m "F$i"
+done
+
+git checkout master
+
+for i in 4 5
+do
+  echo "M$i" > M$i.txt
+  git add M$i.txt
+  git commit -m "M$i"
+done
